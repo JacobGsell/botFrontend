1d3fc6258e1ee59c61cca99685a6d8f6d0b6d183
eichelsohn <jacob.gsell@gmx.de>
2019-12-12 13:35:04 +0100

    Extended js code for builder

--- js/dialog.js
+++ js/dialog.js
@@ -1,12 +1,50 @@
-// Füge Konversationsblock ein
+class topic {
+    constructor(prev, next) {
+        this.prev = prev;
+        this.next = next;
+    }
+    show() {
+        let selects = document.querySelectorAll('select');
+        let prev = selects[0].querySelector('option');
+        let next = selects[1].querySelector('option');
+
+        prev.innerHTML = this.prev;
+        next.innerHTML = this.next;
+    }
+}
+
+const NewBlock = makeAddBlock();
+
+// Add conversation block
 function addConvoBlock() {
     let tmp = document.createElement('div');
     let p = document.createElement('P');
     let main = document.querySelector('main');
-    
+
     p.innerHTML = "Neuer Dialog";
     tmp.className = "convo_block";
 
     tmp.appendChild(p);
-    main.prepend(tmp);
+    main.appendChild(tmp);
+
+    document.querySelector('.add_block').remove()
+    main.appendChild(NewBlock);
+}
+
+function makeAddBlock() {
+    let add_block = makeClassedElem('div', 'add_block');
+    let text_wrapper = makeClassedElem('div', 'text-wrapper');
+    let plusIcon = makeClassedElem('i', 'fas fa-plus-circle');
+
+    add_block.appendChild(text_wrapper.appendChild(plusIcon));
+    add_block.onclick = function () { addConvoBlock(); }
+
+    return add_block;
+}
+
+// Helper function for creating stuff
+function makeClassedElem(tag, className) {
+    let div = document.createElement(tag);
+    div.className = className;
+    return div;
 }

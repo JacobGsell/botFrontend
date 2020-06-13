a85ca2376c42e8e49c83b37b4ed660383c681625
JacobGsell <gsellja67901@th-nuernberg.de>
2020-06-13 11:46:33 +0200

    Started tests with json file

--- DASHBOARD/js/util.js
+++ DASHBOARD/js/util.js
@@ -8,3 +8,15 @@ function log(title=null, content)
         console.log(content)
     }
 }
+
+function json2Intent(json) {
+    // Topic:
+    log(json);
+    let botName = json.Metadata.name;
+    log("Name of bot: ", botName);
+    let intents = json.Content.intents;
+    log("Intents: ", intents);
+    let dialogs = intents[0].dialogs;
+    log("Dialog of 0: ", dialogs);
+}
+
